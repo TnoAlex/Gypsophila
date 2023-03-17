bcc4717b28c2de1d21672da93789c7946d0d1073
Nicholas_Alex <alex@njust.edu.cn>
2023-03-17 15:13:18 +0800

    样例添加

---
+++ sample/sample_source.cc
@@ -0,0 +1,29 @@
+func main(int f):int{
+	plural p = 2+3i;
+	float f = 13.7e-9;
+	int c,d;
+	a = 10 /3;
+	b = 13;
+	c,d=a;
+	bool q=true;
+	ll[1] =10;
+	int a,b;
+	int ll[12];
+	if(q){
+		bool v = a>b;
+	}else{
+		bool k = v && q; 
+	}
+	while(a<5){
+		b = b+1; 
+	}
+	for(int i=0;i<10;i=i+1){
+		a = a+1;
+	}	
+	test(c);
+	return c;
+}
+func test(int s):int{
+	int l = main(s);
+	return l;
+}
